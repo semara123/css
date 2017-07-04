b653f2b669f0ca5d0a4a9069b1ae499f65f99958
Putu Gede Semara Putra <semaraincorporated@gmail.com>
2017-07-04 09:07:10 -0700

    ss

--- js/index.js
+++ js/index.js
@@ -99,7 +99,7 @@ var ringer = {
     $r.ctx.stroke();
     
     // label
-    $r.ctx.fillStyle = "#d50000";
+    $r.ctx.fillStyle = "#009688";
    
     $r.ctx.font = '12px Helvetica';
     $r.ctx.fillText(label, 0, 23);
